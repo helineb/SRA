abdc673a3b095b824a469b6e55dc07212802cccd
helineb <heline.bernier@gmail.com>
2018-02-15 09:48:57 +0100

    Configuration projet

---
+++ SRA/SRA/Models/TypeAnimal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SRA.Models
+{
+    public class TypeAnimal
+    {
+        [Key]
+        public Guid id { get; set; }
+        public String libelle { get; set; }
+        public String description { get; set; }
+    }
+}
---
+++ SRA/SRA/Models/bddSRAContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SRA.Models
+{
+    public class bddSRAContext : DbContext
+    {
+        public bddSRAContext() : base("bddSRA")
+        {
+
+        }
+        public DbSet<TypeAnimal> TypeAnimals { get; set; }
+    }
+}
