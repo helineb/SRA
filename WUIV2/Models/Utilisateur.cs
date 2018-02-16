8a9dd6175dff9184d975fec26ab90f999f84b02f
helineb <heline.bernier@gmail.com>
2018-02-16 10:07:34 +0100

    énumération BDD

--- WUIV2/Models/Utilisateur.cs
+++ WUIV2/Models/Utilisateur.cs
@@ -12,7 +12,7 @@ namespace WUIV2.Models
         public int id { get; set; }
         public String mail { get; set; }
         public String mdp { get; set; }
-        public String role { get; set; }
+        public Role role { get; set; }
         public virtual List<Message> messages { get; set; }
 
         public Utilisateur()
